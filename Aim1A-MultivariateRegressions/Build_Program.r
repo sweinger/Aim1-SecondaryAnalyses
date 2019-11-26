48633f3f5eea79338d6a63771e16e9f8cac1be7d
Katrina M. Kaiser <52082418+kmkaiser@users.noreply.github.com>
2019-11-26 14:58:31 -0800

    Create Build_Program.r

---
+++ Aim1A-MultivariateRegressions/Build_Program.r
@@ -0,0 +1,7 @@
+# Build Analytical File
+
+## Steps: Merge on value sets:
+## Merge on abx_inappropriate by diagnosis code
+## Merge on region by state - patient, consult
+## Merge on is_abx by prescriptions 1-3 if not provided by TD?
+## Create cancellations flags 
