00a495ef21249f52a3bf077f000dfba00afa1fcd
Katrina M. Kaiser <52082418+kmkaiser@users.noreply.github.com>
2019-11-27 09:49:44 -0800

    Update and rename Build_Program.r to Aim1A_Build_Program.r

--- Aim1A-MultivariateRegressions/Aim1A_Build_Program.r
+++ Aim1A-MultivariateRegressions/Aim1A_Build_Program.r
@@ -1,6 +1,9 @@
 # Build Analytical File
 
 ## Steps: Merge on value sets:
+
+
+
 ## Merge on abx_inappropriate by diagnosis code
 ## Merge on region by state - patient, consult
 ## Merge on is_abx by prescriptions 1-3 if not provided by TD?
