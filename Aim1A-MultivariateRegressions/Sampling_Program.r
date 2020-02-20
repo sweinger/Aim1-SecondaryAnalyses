6ce86d7e321a53960c7a6a9b79684b1b81a67d1f
Katrina M. Kaiser <52082418+kmkaiser@users.noreply.github.com>
2020-02-20 11:33:56 -0800

    Update Sampling_Program.r

--- Aim1A-MultivariateRegressions/Sampling_Program.r
+++ Aim1A-MultivariateRegressions/Sampling_Program.r
@@ -1,2 +1,45 @@
 # Cut Sample Data to send to USC for program checking
 
+# read in csvs - update the path parameter to reflect location of consult csvs - maybe a subfolder
+ConsultDetails_all <- 
+  list.files(path=, pattern = "*.csv") %>% 
+  map_dfr(~read_csv(.)) %>%
+  filter(Product=="GENMEDICAL")
+
+
+
+#Read in merged ICD list - update path parameter
+ICD_Merged <- read.csv()
+
+#Merge on ARI flag with abx appropriate/inappropriate to diagnoses
+#this file includes everything on both Jeff's and TD's lists, including some non-ARIs
+ConsultDetails_icd <- left_join(ConsultDetails,ICD_Merged,by=c("Diagnosis_1"="diagnosis_cd"),suffix=c("consults","icd"))
+
+#sample some cancellations
+Cancellations <- 
+ConsultDetails_icd %>%
+  filter(Cancelled_Consult=="Y") %>%
+  sample_n(50)
+
+#sample mostly ARIs with prescription
+ARI_Prescription <- 
+ConsultDetails_icd %>%
+  filter(!is.na(diagnosis_cd) & !is.na(Prescription_1)) %>%
+  sample_n(500)
+
+#sample mostly ARIs with no prescription
+ARI_NoDrug <- 
+ConsultDetails_icd %>%
+  filter(!is.na(diagnosis_cd) & is.na(Prescription_1)) %>%
+  sample_n(250)
+
+#sample none of the above
+OtherConsults <- 
+ConsultDetails_icd %>%
+  filter(is.na(diagnosis_cd) & is.na(Prescription_1)) %>%
+  sample_n(200)
+
+SampleConsults <- bind_rows(Cancellations, ARI_Prescription, ARI_NoDrug, OtherConsults)
+
+#export - specify path
+write.csv(SampleConsults)
